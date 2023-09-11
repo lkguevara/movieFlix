cd18e9769069a2ca80fcde4336e8f05d22138016
lkguevara <liann.guevara@gmail.com>
2023-09-11 16:55:18 -0500

    aplicando estilos a peliculas populares y peliculas más votadas

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,12 +7,16 @@ import PopularMovies from '../components/PopularMovies/PopularMovies'
 const Home = () => {
   const newMovies = useFetch(`${URL_API}/movie/now_playing?api_key=${URL_TOKEN}&language=es-US&page=1`)
   const popularMovies = useFetch(`${URL_API}/movie/popular?api_key=${URL_TOKEN}&language=es-US&page=1`)
-  
+  const topRatedMovies = useFetch(`${URL_API}/movie/top_rated?api_key=${URL_TOKEN}&language=es-US&page=1`)
+  console.log(topRatedMovies)
 
   return (
     <div className='carousel'>
       <SliderMovies movies={newMovies}/>
-      <PopularMovies title='Películas Populares' movies={popularMovies}/>
+      <section className='popularMovies'>
+        <PopularMovies title='Películas Populares' movies={popularMovies} showVoteAverage={false}/>
+        <PopularMovies title='Películas Más Votadas' movies={topRatedMovies} showVoteAverage={true}/>
+      </section>
     </div>
   )
 }
--- src/components/PopularMovies/PopularMovies.jsx
+++ src/components/PopularMovies/PopularMovies.jsx
@@ -2,30 +2,39 @@ import Loading from '../Loading/Loading'
 import PropTypes from 'prop-types'
 import './index.scss'
 import { Avatar, List } from 'antd'
+import { Link } from 'react-router-dom'
+import { RightCircleOutlined } from '@ant-design/icons'
 
-const PopularMovies = ({title, movies}) => {
+const PopularMovies = ({title, movies, showVoteAverage}) => {
 
     if(movies.loading || !movies.result){
         return <Loading />
     }
     const {results} = movies.result
-    const popularMovies = results.slice(0,10)
+    const popularMovies = results.slice(0,15)
 
 
     return (
         <List 
-            className='popular-movies'
+            className='popularMovies'
             size='default'
             header={<h2>{title}</h2>}
             bordered
             dataSource={popularMovies}
             renderItem={movie => (
-                <List.Item>
+                <List.Item className='listItem'>
                     <List.Item.Meta
+                        className='listItemMeta'
                         avatar={<Avatar src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} />}
-                        title={movie.title}
-                        // Puedes agregar más detalles de la película aquí
+                        title={<Link to={`/movie/${movie.id}`}>{movie.title}</Link>}
+                        description={showVoteAverage ? <span>{movie.vote_average} / 10  - <span>{movie.release_date.split("-")[0]}</span></span> : null}
+                        
                     />
+                    <Link to={`/movie/${movie.id}`}>
+                        <RightCircleOutlined style={{fontSize: '20px'}} />
+                    </Link>
+                    
+
                 </List.Item>
             )}
         />
@@ -34,7 +43,8 @@ const PopularMovies = ({title, movies}) => {
 
 PopularMovies.propTypes = {
     movies: PropTypes.array.isRequired,
-    title: PropTypes.string.isRequired
+    title: PropTypes.string.isRequired,
+    showVoteAverage: PropTypes.bool.isRequired
 }
 
 export default PopularMovies
